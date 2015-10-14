ed45bb2654fc9d338c1b24197e27387d862614e3
HughParsonage <hugh.parsonage@gmail.com>
2015-10-14 11:46:46 +1100

    to HES

--- savings_by_disposable_income.R
+++ savings_by_disposable_income.R
@@ -4,9 +4,9 @@ purl("GST_costing.Rmd")
 source("GST_costing.R", echo=FALSE)
 file.remove("GST_costing.R")
 
-sih11 %<>%
-  mutate(disposable_income = Total_current_weekly_income_from_all_sources - Imputed_current_weekly_tax_payable_basis)
+hes10hh %>%
+  mutate(disposable_income = Total_current_weekly_income_from_all_sources - Household_weekly_expenditure_on_income_tax_HES_only)
 
 disposable.income.quintiles <- 
-  svydesign(data = sih11, ids = ~Person_number_within_each_income_unit, weights = ~Person_weight) %>%
+  svydesign(data = hes10hh, ids = ~Person_number_within_each_income_unit, weights = ~Person_weight) %>%
   svyquantile(~disposable_income, design = ., quantiles = (0:5)/5)
