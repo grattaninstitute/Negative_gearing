469b8f66ce9da94634c64acf7aeab774c24a0098
HughParsonage <hugh.parsonage@gmail.com>
2015-12-16 22:41:20 +1100

    added prop v amt charts

--- Grattan_gathering/Distr_super_assets_over_60s-HILDA.R
+++ Grattan_gathering/Distr_super_assets_over_60s-HILDA.R
@@ -4,6 +4,9 @@ library(data.table)
 library(dplyr)
 library(magrittr)
 library(tidyr)
+library(ggplot2)
+library(scales)
+library(grattan)
 
 hilda_master <- 
   read.dta("../HILDA/Master_m130c.dta") %>%
@@ -13,8 +16,51 @@ hilda <- read.dta("../HILDA/Household_j130c.dta")
 hilda <- data.table(hilda)
 
 hilda %>%
-  select(jhwsupei, # Superannuation 
-         jhwfini,
-         jhwassei,
-         jhgage1)
-hilda[,1:5,with=F]
+  select(
+    jhhrhid,
+    jhhwth,
+    jhwsupei, # Superannuation 
+    jhwfini, # financial assets
+    jhwassei, # other assets
+    age = jhgage1
+  ) %>% 
+  mutate(
+    prop_super = jhwfini / jhwassei
+  ) %>%
+#   group_by(age) %>%
+#   summarise(prop_super = weighted.mean(prop_super, jhhwth)) %>%
+  #
+  grplot(aes(x = age, y = prop_super, group = 1, weight = jhhwth)) + 
+  stat_smooth(color = Orange, size = 1.5) +
+  scale_y_continuous(label=percent)
+
+hilda %>%
+  grplot(aes(x = jhgage1, y = jhwsupei)) + 
+  geom_point(aes(alpha = jhhwth/max(jhhwth)))
+
+
+# ====
+hilda %>%
+  select(
+    jhhrhid,
+    jhhwth,
+    jhwsupei, # Superannuation 
+    jhwfini, # financial assets
+    jhwassei, # other assets
+    age = jhgage1
+  ) %>% 
+  mutate(
+    prop_super = jhwfini / jhwassei,
+    age_group = factor(cut(age, breaks = c(-Inf, 18, 64, Inf))) 
+  ) %>%
+  #   group_by(age) %>%
+  #   summarise(prop_super = weighted.mean(prop_super, jhhwth)) %>%
+  #
+  grplot(aes(x = jhwassei, y = prop_super, weight = jhhwth)) + 
+  # stat_smooth(color = Orange, size = 1.5) +
+  stat_smooth(aes(color = age_group, group = age_group),
+              size = 1.5, fill = NA) + 
+  scale_y_continuous(label=percent) + 
+  scale_color_manual(values = c(theGrey, gpal(4))) + 
+  theme(legend.position = c(0.7, 0.8))
+
