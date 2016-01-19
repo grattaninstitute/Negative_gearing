0aac4d96f4813abc4e0c8b4cbb3600102203c518
HughParsonage <hugh.parsonage@gmail.com>
2016-01-19 15:08:50 +1100

    replacement rate
    The function now produces a reasonable output, but it's unclear whether
    the parameters are appropriate.
    
    TODO: Differing annuity rates and accumulation rates in retirement

--- Grattan_gathering/replacement_rate.R
+++ Grattan_gathering/replacement_rate.R
@@ -10,10 +10,11 @@
 library(data.table)
 library(dplyr)
 
-replacement_rate <- function(income_initial, 
+replacement_rate_super <- function(income_initial, 
                              age_initial = 32,
                              year_initial = 2015,
                              retirement_age = 67,
+                             termination_age = retirement_age + annuity_period,
                              SG_policy = "2014-15 Budget",  # Legislated to rise to 12%
                              CPI = 0.025,
                              real_wage_growth = 0.015,
@@ -23,16 +24,18 @@ replacement_rate <- function(income_initial,
                              annuity_rate = 0.04,
                              annuity_period = 25
 ){
-  if(SG_policy == "2014-15 Budget"){
-    Tbl <- data.table(
+    Accumulation <- data.table(
       Age = seq(age_initial, retirement_age + annuity_period, by = 1L),
       Super_balance = 0
     ) %>%
       mutate(Year = (year_initial - 1L) + 1:n(),
-             SG_rate = ifelse(Year <= 2020, 
-                              0.095, 
-                              pmin(0.12, 
-                                   0.095 + 0.005 * (Year - 2020))),
+             SG_rate = ifelse(SG_policy == "2014-15 Budget",
+                              ifelse(Year <= 2020, 
+                                     0.095, 
+                                     pmin(0.12, 
+                                          0.095 + 0.005 * (Year - 2020))),
+                              ifelse(SG_policy == "9.5%",
+                                     0.095, NA_real_)),
              Salary = income_initial * (1 + real_wage_growth + CPI) ^ (Year - year_initial),
              contributions = Salary * SG_rate * (1 - contributions_tax),
              Retd = Age > retirement_age)
@@ -42,21 +45,81 @@ replacement_rate <- function(income_initial,
       prev_bal + prev_bal * super_rate_of_return * (1 - earnings_tax_effective) + contribution
     }
     
-    Tbl$Super_balance <- Reduce(f = balance_now, 
-                                x = Tbl$contributions, 
-                                accumulate = TRUE)
+    Accumulation$Super_balance <- Reduce(f = balance_now, 
+                                         x = Accumulation$contributions, 
+                                         accumulate = TRUE)
     
-    Tbl[ ,Super_balance_real := Super_balance / (1 + CPI + real_wage_growth) ^ (Year - year_initial))]
+    Accumulation[ ,Super_balance_real := Super_balance / (1 + CPI + real_wage_growth) ^ (Year - year_initial)]
     
-    balance_retd <- function(prev_bal, withdrawal){
-      # r_a rate of annuity inflation
-      # e_r earnings rate in retirement)
-      (prev_bal - withdrawal * (1 + r_a)) * (1 + e_r)
+    # Use wolfram alpha to solve ther difference equation
+    # (which I could once do but WA is better)
+    br <- function(initial_balance, 
+                   n_years, 
+                   annuity_initial, 
+                   rate # both annuity and earnings
+    ){
+      ifelse(n_years == 0,
+             initial_balance,
+             (1 + rate)^(n_years) * (initial_balance - (1 + rate) * annuity_initial * n_years))
     }
     
+    
+    
+    balance_retirement <- function(...){
+      pmax(0, br(...))
+    }
+    
+    final_accumulated_balance <- 
+      Accumulation %>%
+      dplyr::filter(Age == retirement_age) %$%
+      Super_balance
+    
+    annuity_initial <- 
+      uniroot(f = function(x) br(final_accumulated_balance, 
+                                 n_years = termination_age - retirement_age,
+                                 x,
+                                 rate = annuity_rate),
+              interval = c(1, final_accumulated_balance)) %$%
+      root
+    
+    Retirement <- 
+      data.table(Age = seq(retirement_age + 1, termination_age)) %>%
+      mutate(Retirement_balance = balance_retirement(initial_balance = final_accumulated_balance,
+                                                     n_years = Age - retirement_age,
+                                                     annuity_initial = annuity_initial,
+                                                     rate = annuity_rate),
+             Annuity = annuity_initial * (1 + annuity_rate)^(1:n() - 1)
+      )
+  .debug <- merge(Accumulation, Retirement, by = "Age", all.x = TRUE, all.y = TRUE)
+
+  last_salary <- 
+    merge(Accumulation, Retirement, by = "Age", all.x = TRUE, all.y = TRUE) %>%
+    mutate(Balance = ifelse(Retd, Retirement_balance, Super_balance)) %>%
+    select(-Super_balance, -Retirement_balance) %>%
+    filter(Retd, !lag(Retd)) %$%
+    {Salary}
+  
+  first_annuity <- 
+    merge(Accumulation, Retirement, by = "Age", all.x = TRUE, all.y = TRUE) %>%
+    mutate(Balance = ifelse(Retd, Retirement_balance, Super_balance)) %>%
+    select(-Super_balance, -Retirement_balance) %>%
+    filter(Retd, !lag(Retd)) %$%
+    {Annuity}
+  
+  age_pension <- function(income = first_annuity * (1 + CPI)^(age_initial - retirement_age),
+                          assets = 0){
+    fortnightly <- 788.40 - ifelse(income/26 < 162, 
+                                   0, 
+                                   ifelse(income/26 > 1896, 
+                                          788.40,
+                                          income/26 * 0.5))
+    fortnightly * 26
   }
-  super_income <- 
-    Tbl %$%
-    last(Super_balance_real) / ((1 - 1/(1 + annuity_rate)^annuity_period)/annuity_rate)
-  Tbl 
+  
+  .debug <<-
+    .debug %>%
+    mutate(age_pension = age_pension(income = Annuity/(1 + CPI)^(Age - age_initial)))
+  
+  (first_annuity + age_pension()) / income_initial
 }
+
